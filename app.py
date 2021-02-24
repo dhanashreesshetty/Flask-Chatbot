ddfa3bf816546a2ab214fcc2d6e9e78039092df8
Dhanashree Shetty <2017.dhanashree.shetty@ves.ac.in>
2021-02-24 18:26:13 +0530

    Added home page

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from chatbot import chatbot
 from flask import Flask, render_template, request
+from senti.nb import remove_pattern
 
 app = Flask(__name__)
 app.static_folder = 'static'
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    print(1)
+    print(remove_pattern("@usa", "@[\w]*"))
+    return render_template("home.html")
 
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
     return str(chatbot.get_response(userText))
+
+@app.route("/chatbot")
+def show_chatbot():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run() 
